92d6513f8cb5f1ff5782e14a5eb25602bf45e2fd
Prince Bansal <prince.bansal008@gmail.com>
2023-09-20 23:20:39 +0530

    reduce() array helper updated

--- ES6-JS/Array-Helper-Methods/6.reduce.js
+++ ES6-JS/Array-Helper-Methods/6.reduce.js
@@ -57,6 +57,7 @@ function isBalanced(stringParanthesis) {
   // we will decrease the accumulator once we find starting part of braces pair
 
   return !stringToArray.reduce(function (accumulator, char) {
+    if (accumulator < 0) return accumulator;
     if (char === "{" || char === "(" || char === "[") {
       return ++accumulator;
     } else if (char === "}" || char === ")" || char === "]") {
@@ -66,6 +67,75 @@ function isBalanced(stringParanthesis) {
   }, 0);
 }
 
-console.log(isBalanced("{{}}"));
-console.log(isBalanced("[[[[{{{]]]]"));
-console.log(isBalanced("{{{{{[[[(())}}]}}]}]"));
+console.log(isBalanced("{{}}")); // true
+console.log(isBalanced("[[[[{{{]]]]")); // false
+console.log(isBalanced("{{{{{[[[(())}}]}}]}]")); // true
+console.log(isBalanced("())(")); // false
+console.log(isBalanced("][")); // false (incorrect order)
+
+console.log();
+
+// Eg-1
+var trips = [{ distance: 34 }, { distance: 12 }, { distance: 1 }];
+
+var totalDistance = trips.reduce(function (acc, trip) {
+  return acc + trip.distance;
+}, 0);
+console.log(totalDistance);
+
+// Eg-2: Use the 'reduce' helper to create an object that tallies the number of sitting and standing desks.
+
+var desks = [
+  { type: "sitting" },
+  { type: "standing" },
+  { type: "sitting" },
+  { type: "sitting" },
+  { type: "standing" },
+];
+
+var deskTypes = desks.reduce(
+  function (accumulatorObj, desk) {
+    if (desk.type === "sitting") {
+      accumulatorObj.sitting++;
+    } else {
+      accumulatorObj.standing++;
+    }
+    return accumulatorObj;
+  },
+  { sitting: 0, standing: 0 }
+);
+
+console.log(deskTypes);
+
+console.log();
+console.log();
+/*
+Eg-3:Write a function called 'unique' that will remove all the duplicate values from an array.
+
+For example, given the following array:
+var numbers = [1, 1, 2, 3, 4, 4];
+Your function should return
+[1, 2, 3, 4]
+Hint: Use the 'reduce' and 'find' helpers.   
+
+*/
+
+var numbers = [1, 1, 2, 3, 4, 4, 69, 69, 69];
+
+function unique(numbers) {
+  return numbers.reduce(function (accumulatorArray, num) {
+    // using find(), we will check whether number (num) in final answer array (accumulatorArray) exists or not (by comparing given array element (num) and accumulatorArray element (targetVal))
+    // => if exits : do not push
+    // => if doesn't exists : push it
+    var existingNum = accumulatorArray.find(function (targetVal) {
+      return num === targetVal;
+    });
+    // console.log(accumulatorArray);
+    if (!existingNum) {
+      accumulatorArray.push(num);
+    }
+    return accumulatorArray;
+  }, []);
+}
+
+console.log(unique(numbers));
