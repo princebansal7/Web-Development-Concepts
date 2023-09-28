693ad8357317cd25cfa68be5f5a7e3e9d1ed21ae
Prince Bansal <prince.bansal008@gmail.com>
2023-09-28 14:40:59 +0530

    updated

--- ES6-JS/1.let-const.js
+++ ES6-JS/1.let-const.js
@@ -62,16 +62,16 @@ for (var i = 0; i < statuses.length; i++) {
 */
 
 const statuses = [
-  { code: "OK", response: "Request successful" },
-  { code: "FAILED", response: "There was an error with your request" },
-  { code: "PENDING", response: "Your reqeust is still pending" },
+    { code: "OK", response: "Request successful" },
+    { code: "FAILED", response: "There was an error with your request" },
+    { code: "PENDING", response: "Your reqeust is still pending" },
 ];
 let message = "";
 let currentCode = "OK";
 
 for (let i = 0; i < statuses.length; i++) {
-  if (statuses[i].code === currentCode) {
-    message = statuses[i].response;
-  }
+    if (statuses[i].code === currentCode) {
+        message = statuses[i].response;
+    }
 }
 console.log(message);
