91363554ac7e235958fd47a4ea193c78e00354bc
Prince Bansal <prince.bansal008@gmail.com>
2024-07-14 14:24:25 +0530

    files-renamed

--- ES6-JS/8.classes.js
+++
@@ -1,4 +0,0 @@
-// Js don't have Object-Inheritance
-// It have Prototype-Inheritance.
-
-// ES6 solves this using new feature called 'classes' but under the hood it's still uses Prototype inheritance
