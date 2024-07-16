725eb726e6b8f9e940ef284ebb1314fd4b75a617
Prince Bansal <prince.bansal008@gmail.com>
2024-07-17 03:41:03 +0530

    comment fixed

--- frontend-backend/example-1/backend.js
+++ frontend-backend/example-1/backend.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const app = express();
 
 // We know nothing about CORS yet, still
-// it clear it's working as a middleware to pre-check something,
+// its clear it's working as a middleware to pre-check something,
 // will learn more later
 app.use(cors());
 
