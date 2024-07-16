68e2f684998bfdc609ce2cc7e13983026e3c43d4
Prince Bansal <prince.bansal008@gmail.com>
2024-07-17 02:04:19 +0530

    fullstack flow with example added & files moved

--- frontend-backend/example-1/backend.js
+++ frontend-backend/example-1/backend.js
@@ -2,6 +2,9 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
+// We know nothing about CORS yet, still
+// it clear it's working as a middleware to pre-check something,
+// will learn more later
 app.use(cors());
 
 app.get("/sum", function (req, res) {
