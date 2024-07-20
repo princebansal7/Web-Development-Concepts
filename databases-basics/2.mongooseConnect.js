f495d72fe7f964b9ba3d0eaf75a14919076c303f
Prince Bansal <prince.bansal008@gmail.com>
2024-07-21 00:24:06 +0530

    comment fixed

--- databases-basics/2.mongooseConnect.js
+++ databases-basics/2.mongooseConnect.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 // connecting with mongoose DB, where first_mongo_db is database name
 mongoose.connect(`${process.env.DB_CONNECTION_URL}/first_mongo_db`);
 
-// Defining Scehma to let mongo know what kinda data is coming
+// Defining Schema to let mongo know what kinda data is coming
 // - here Test_User_Table is table name inside the first_mongo_db database
 const TestUser = mongoose.model("Test_User_Table", {
     name: String,
