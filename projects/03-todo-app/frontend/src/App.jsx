fb35da2f4eb34596aa21b7435d91e14326070ba9
Prince Bansal <prince.bansal008@gmail.com>
2024-07-30 03:17:05 +0530

    why wrong way-reason added

--- projects/03-todo-app/frontend/src/App.jsx
+++ projects/03-todo-app/frontend/src/App.jsx
@@ -3,14 +3,18 @@ import { useState } from "react";
 import Todos from "./components/Todos";
 
 function App() {
-    // This way whenever get get new data using fetch, state is updated =>
-    // setTodoBE again re-renders App() => again fetch() => again state update and so on..
-    // that's why this is not the correct way to send backend request to get data
+    // 1. This way whenever get get new data using fetch, state is updated =>
+    //    setTodoBE again re-renders App() => again fetch() => again state update and so on..
+    //    that's why this is not the correct way to send backend request to get data
+    //
     const [todosBE, setTodosBE] = useState([]);
     fetch("http://localhost:3000/todos").then(async response => {
         const json = await response.json();
         setTodosBE(json.todos);
     });
+    //
+    // to Solve this we need to use useEffect() use, which solves this issue
+
     return (
         <>
             <CreateToDo />
