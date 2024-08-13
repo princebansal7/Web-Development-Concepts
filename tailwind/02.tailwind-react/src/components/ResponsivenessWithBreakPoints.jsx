c2d6dcc64358b875bc06945935a41b82c6e721af
Prince Bansal <prince.bansal008@gmail.com>
2024-08-14 01:15:53 +0530

    tailwind - [IMP] breakpoint & responsiveness concept done - more responsiveness

--- tailwind/02.tailwind-react/src/components/ResponsivenessWithBreakPoints.jsx
+++ tailwind/02.tailwind-react/src/components/ResponsivenessWithBreakPoints.jsx
@@ -6,7 +6,7 @@ export function ResponsivenessWithBreakPoints() {
             </div>
             <br />
             <br />
-            <div className="grid grid-cols-1 md:grid-cols-4">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
                 <div style={{ backgroundColor: "lightblue", color: "blue" }}>
                     tailwind - flex - Child-0
                 </div>
