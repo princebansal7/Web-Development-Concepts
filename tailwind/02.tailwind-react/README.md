64def6b56ae8b0be75ef34f4e813ae052615faf0
Prince Bansal <prince.bansal008@gmail.com>
2024-08-13 23:43:08 +0530

    tailwind - flex, grid basic done

--- tailwind/02.tailwind-react/README.md
+++ tailwind/02.tailwind-react/README.md
@@ -1,3 +1,7 @@
-## Install tailwind for vite
+## tailwind for vite
 
-- [refer link](https://tailwindcss.com/docs/guides/vite)
+
+- **flex**: deals with spaces between the elements
+- **grid**: deals with how many elements needs to be in single line based on the current width of the page 
+- both helps in responsiveness
+- How to install tailwind in vite react [refer link](https://tailwindcss.com/docs/guides/vite)
--- tailwind/02.tailwind-react/src/App.jsx
+++ tailwind/02.tailwind-react/src/App.jsx
@@ -1,29 +1,12 @@
 import "./App.css";
+import { Flex } from "./components/Flex";
+import { Grid } from "./components/Grid";
 
 function App() {
     return (
-        // 1. html way but in react
         <>
-            <div style={{ display: "flex" }}>
-                <div style={{ backgroundColor: "lightblue" }}>I am Child-1</div>
-                <div style={{ backgroundColor: "yellow" }}>I am Child-2</div>
-                <div style={{ backgroundColor: "lightgrey" }}>I am Child-3</div>
-            </div>
-            <hr />
-
-            <div style={{ display: "flex", justifyContent: "flex-start" }}>
-                <div style={{ backgroundColor: "lightblue" }}>I am Child-1</div>
-                <div style={{ backgroundColor: "yellow" }}>I am Child-2</div>
-                <div style={{ backgroundColor: "lightgrey" }}>I am Child-3</div>
-            </div>
-            <hr />
-
-            <div style={{ display: "flex", justifyContent: "space-around" }}>
-                <div style={{ backgroundColor: "lightblue" }}>I am Child-1</div>
-                <div style={{ backgroundColor: "yellow" }}>I am Child-2</div>
-                <div style={{ backgroundColor: "lightgrey" }}>I am Child-3</div>
-            </div>
-            <hr />
+            {/* <Flex /> */}
+            <Grid />
         </>
     );
 }
