96362c3c4016931e3c46eebafd27d93c2e5c4b6d
Prince Bansal <75045465+princebansal7@users.noreply.github.com>
2024-08-24 16:25:34 +0530

    lifecycle events with functinal components and useEffect explored

---
+++ react-js/28.react-custom-hooks/01.class-vs-function-lifecycle/src/App.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+// Let's unmount the component after 5 secs
+// To demonstrate life-cycle event using useEffect()
+
+function App() {
+    const [render, setRender] = useState(true);
+
+    // After 5 seconds it will change the state to false
+    // we can see then component gets unmounted in console too
+
+    // useEffect(() => {
+    //     setTimeout(() => {
+    //         setRender(false);
+    //     }, 5000);
+    // }, []);
+
+    // To mount, unmount component after every 5 sec
+    useEffect(() => {
+        setInterval(() => {
+            setRender(ren => !ren);
+        }, 5000);
+    }, []);
+
+    return <>{render ? <CounterFunctionComponent /> : <></>}</>;
+}
+
+function CounterFunctionComponent() {
+    useEffect(() => {
+        console.log("Component mounted");
+
+        return () => {
+            console.log("Component unmounted");
+        };
+    }, []);
+
+    return <>Inside Main component</>;
+}
+
+export default App;
