26619e3ea186efd606d320910a70d2018d29df61
Prince Bansal <75045465+princebansal7@users.noreply.github.com>
2024-08-25 17:23:42 +0530

    TypeScript basics added

---
+++ typescript/01.tsc-basics/02.secondTS.ts
@@ -0,0 +1,6 @@
+
+// giving type to variable before hand just like any strongly type languages
+
+let y: number = 69
+// y = "prince" // will give error while compiling & won't create js file
+console.log(y);
