7e126546ac1fa63023cc8757b3ebbde00ad7bb49
Prince Bansal <75045465+princebansal7@users.noreply.github.com>
2024-08-25 15:11:08 +0530

    correctly fetching data from backend now

--- projects/03-todo-app/frontend/src/App.jsx
+++ projects/03-todo-app/frontend/src/App.jsx
@@ -9,6 +9,11 @@ function App() {
     //    setTodoBE again re-renders App() => again fetch() => again state update and so on..
     //    that's why this is not the correct way to send backend request to get data
 
+    // fetch("http://localhost:3000/todos").then(async response => {
+    //     const json = await response.json();
+    //     setTodosBE(json.todos);
+    // });
+
     // To Solve this we need to use useEffect() use, which solves this issue
 
     useEffect(() => {
