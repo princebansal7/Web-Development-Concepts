0f99c3303f71f8410ac678692373a02c4b64108e
Prince Bansal <75045465+princebansal7@users.noreply.github.com>
2024-09-04 04:18:36 +0530

    typos fixed

--- server-code/09.calRequestsMiddleware.js
+++ server-code/09.calRequestsMiddleware.js
@@ -2,24 +2,24 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-let numberOfReqests = 0;
-// commonly used middleware to count request comming on a route
+let numberOfRequests = 0;
+// commonly used middleware to count request coming on a route
 function calculateRequests(req, res, next) {
-    numberOfReqests++;
-    console.log("Requests: ", numberOfReqests);
+    numberOfRequests++;
+    console.log("Requests: ", numberOfRequests);
     next();
 }
 
 app.get("/check-req", calculateRequests, (req, res) => {
     res.send(
-        `<h2>Calculating Number of requests /check-req route using middleware: ${numberOfReqests}</h2>`
+        `<h2>Calculating Number of requests /check-req route using middleware: ${numberOfRequests}</h2>`
     );
 });
 
 // If I add new route with same middleware, request count will be continued with same number..
 app.get("/prince", calculateRequests, (req, res) => {
     res.send(
-        `<h2>Calculating Number of requests on prince/ route using middleware: ${numberOfReqests}</h2>`
+        `<h2>Calculating Number of requests on prince/ route using middleware: ${numberOfRequests}</h2>`
     );
 });
 app.listen(port, () => {
